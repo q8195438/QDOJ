dba88c2c1632e10eacb9d585b1b425ea4c106351
ZAKERR <815608963@qq.com>
2018-01-30 11:23:41 +0800

    未调用dbhelper的方法

--- controller/home.js
+++ controller/home.js
@@ -14,33 +14,25 @@ module.exports = {
   },
   
   signup: async(ctx, next) => {            
-    let {email,sessionID,username,password} = ctx.request.body    //从客户端获取到的参数 ,session 为null？
+    let params = ctx.request.body    //从客户端获取到的参数 ,session 为null？
 	
-    console.log(`${email} ${sessionID} ${username} ${password}`);
-	if(email == null  || username == null || password == null){
-		console.log("无输入");
-	}
-	else {  
-		let  statu = await userDb.findUser(username);
-	  
-			if(statu){
-			   console.log("用户已存在")
-			   }
-			 else{
-				
-				let tUser = new User({
-				username1:username,
-				password1:password,
-				session1:sessionID,
-				email1:email,
-				});
-				
-				await tUser.save()
-
-				await ctx.redirect("/",{title:username}) 
-			}
-
-		 }	
+     let statu = await User.find({username:params.username})
+	 
+	 if(statu.length != 0){
+		 console.log(statu);
+	 }
+	 
+	 else{
+		 let tUser = new User({
+			 username:params.username,
+			 password:params.password,
+			 session:params.sessionID,
+			 email:params.email,
+		 }) 
+		 
+		 await tUser.save();
+		 await ctx.redirect('/')
+	 }
    }
    
 }
